2ac5f4d8f08541a1c8f84b1ed8e28b796571db26
lukasnxyz <lukasnitzsche@yahoo.com>
2024-07-22 17:38:20 -0500

    update example to make clearer

--- example_tokenize.py
+++ example_tokenize.py
@@ -7,11 +7,16 @@ if __name__ == '__main__':
         corpus = f.read().split('\n') # input needs to be a list of strings
         tks = Tokens(corpus, 1000) # vocab, splits, merges are automatically processed
 
+    # example on single string
     test_str = 'the earth is round and so is the sun, but a cube is not.'
     tokenized = tks.tokenize(test_str)
     encoded = tks.encode(tokenized)
-
     print(tokenized)
     print(encoded)
-    print(tks.decode(encoded))
-    print(tks.detokenize(tks.decode(encoded))) 
+    print(tks.decode(encoded), tks.detokenize(tks.decode(encoded)))
+
+    # example on whole data set
+    tokenized = [tks.tokenize(s) for s in tks.corpus]
+    encoded = [tks.encode(t) for t in tokenized]
+    print(tokenized[:5])
+    print(encoded[:5])
