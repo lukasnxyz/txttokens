afab07a8e4629aa6c8c2910a421114c99feea333
lukasnxyz <lukasnitzsche@yahoo.com>
2024-07-23 11:41:51 -0500

    include newline chars as well

--- example_tokenize.py
+++ example_tokenize.py
@@ -3,20 +3,21 @@ from tokens import Tokens
 if __name__ == '__main__':
     # https://gist.github.com/lukasnxyz/d7b29398dd3b3d1c1dcb14f1f19e3744
     # truths.txt has ~4861 unique words
-    with open ('data/truths.txt', 'r', encoding='utf-8') as f: 
-        corpus = f.read().split('\n') # input needs to be a list of strings
+    with open ('data/tiny_shakespeare.txt', 'r', encoding='utf-8') as f: 
+        corpus = f.read()
         tks = Tokens(corpus, 1000) # vocab, splits, merges are automatically processed
 
     # example on single string
-    test_str = 'the earth is round and so is the sun, but a cube is not.'
+    test_str = 'the earth is round and so is the sun\nbut a cube is not.'
     tokenized = tks.tokenize(test_str)
     encoded = tks.encode(tokenized)
     print(tokenized)
     print(encoded)
-    print(tks.decode(encoded), tks.detokenize(tks.decode(encoded)))
+    print(tks.decode(encoded))
+    print(tks.detokenize(tks.decode(encoded)))
 
     # example on whole data set
-    tokenized = [tks.tokenize(s) for s in tks.corpus]
-    encoded = [tks.encode(t) for t in tokenized]
-    print(tokenized[:5])
-    print(encoded[:5])
+    #tokenized = [tks.tokenize(s) for s in tks.corpus]
+    #encoded = [tks.encode(t) for t in tokenized]
+    #print(tokenized[:5])
+    #print(encoded[:5])
--- tokens.py
+++ tokens.py
@@ -1,7 +1,6 @@
 # adpated from: https://huggingface.co/learn/nlp-course/en/chapter6/5
 from transformers import AutoTokenizer # TODO: implement this custom as well
 from collections import defaultdict
-from tqdm import tqdm
 
 class Tokens:
     def __init__(self, corpus: list, g_vocab_size: int=100):
@@ -18,13 +17,12 @@ class Tokens:
         self.encode = lambda s: [self.stoi[c] for c in s]
         self.decode = lambda e: ''.join([self.itos[i] for i in e])
 
-        self.detokenize = lambda ts: ''.join(t.replace('Ġ', ' ') for t in ts)
+        self.detokenize = lambda ts: ''.join(t.translate(str.maketrans({'Ġ': ' ', 'Ċ': '\n'})) for t in ts)
 
     def _get_word_freqs(self):
         word_freqs = defaultdict(int)
-        for txt in self.corpus:
-            for word, _ in self.tokenizer.backend_tokenizer.pre_tokenizer.pre_tokenize_str(txt):
-                word_freqs[word] += 1
+        for word, _ in self.tokenizer.backend_tokenizer.pre_tokenizer.pre_tokenize_str(self.corpus):
+            word_freqs[word] += 1
         return word_freqs
 
     def _compute_pair_freqs(self):
@@ -47,17 +45,17 @@ class Tokens:
 
     def _train(self):
         merges = defaultdict(str)
-        with tqdm(total=self.g_vocab_size) as pbar:
-            while len(self.vocab) < self.g_vocab_size:
-                pair_freqs = self._compute_pair_freqs()
-                best_pair = max(pair_freqs, key=pair_freqs.get)
-                self._merge_pair(*best_pair)
-                merges[best_pair] = ''.join(best_pair)
-                self.vocab.append(''.join(best_pair))
-                pbar.update(1)
-            return merges
+        while len(self.vocab) < self.g_vocab_size:
+            print(f'{len(self.vocab)}/{self.g_vocab_size}', end='\r', flush=True)
+            pair_freqs = self._compute_pair_freqs()
+            best_pair = max(pair_freqs, key=pair_freqs.get)
+            self._merge_pair(*best_pair)
+            merges[best_pair] = ''.join(best_pair)
+            self.vocab.append(''.join(best_pair))
+        return merges
 
-    # check if str or list of strs
+    # TODO: check if str or list of strs
+    # TODO: some sort of progress
     def tokenize(self, txt: str):
         splits = [list(w) for w, _ in self.tokenizer.backend_tokenizer.pre_tokenizer.pre_tokenize_str(txt)]
         for pair, merge in self.merges.items():
